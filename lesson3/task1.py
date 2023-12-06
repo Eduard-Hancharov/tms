f815e2e586dbc49a3af0fec049246ee572d7f6bf
Eduard-Hancharov <134201656+Eduard-Hancharov@users.noreply.github.com>
2023-12-06 17:24:58 +0300

    Delete lesson3 directory

--- lesson3/task1.py
+++
@@ -1,9 +0,0 @@
-word1 = 'Пайтон'
-word2 = 'Питон'
-print(word1 + ' ' + word2)
-word1, word2 = word2, word1
-print('!' + word1 + ' ' + '!' + ' ' + word2 + '!')
-print(f"!{word1} ! {word2}!")
-print('!{0} ! {1}!'.format(word1, word2))
-
-
--- lesson3/task4(2).py
+++
@@ -1,7 +0,0 @@
-number = int(input('Введите цифру'))
-sum = 0
-i = 1
-while i<=number:
-    sum +=i**3
-    i += 1
-print(sum)
