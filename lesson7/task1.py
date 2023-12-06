ad51459268c04291cea0ba0bf766da8fe685c5af
Eduard-Hancharov <134201656+Eduard-Hancharov@users.noreply.github.com>
2023-12-06 17:25:27 +0300

    Delete lesson7 directory

--- lesson7/task1.py
+++
@@ -1,2 +0,0 @@
-lamb_func = lambda a : f"{a} Четное" if a %2==0 else f"{a} Нечетное"
-print(lamb_func(9))
--- lesson7/task2.py
+++
@@ -1,3 +0,0 @@
-list_1 = [10, 20, 30, 40, 50, 60]
-result = list(map(lambda a: str(a), list_1))
-print(result)
